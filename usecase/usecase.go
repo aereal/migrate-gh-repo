08d6ef9d75464a7103c4b08cb7b7bcbeb5dfdf65
aereal <aereal@aereal.org>
2019-09-27 17:05:38 +0900

    send actual request

--- usecase/usecase.go
+++ usecase/usecase.go
@@ -153,6 +153,10 @@ type createIssueRequest struct {
 
 func (r *createIssueRequest) Do(ctx context.Context, ghClient *github.Client) error {
 	log.Printf("create issue on %s/%s: %#v", r.owner, r.repo, r.issueReq)
+	_, _, err := ghClient.Issues.Create(ctx, r.owner, r.repo, r.issueReq)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -166,6 +170,10 @@ type createIssueCommentRequest struct {
 func (r *createIssueCommentRequest) Do(ctx context.Context, ghClient *github.Client) error {
 	issueComment := &github.IssueComment{Body: &r.body}
 	log.Printf("create issue comment on %s/%s#%d issueComment=%s", r.owner, r.repo, r.issueNumber, issueComment)
+	_, _, err := ghClient.Issues.CreateComment(ctx, r.owner, r.repo, r.issueNumber, issueComment)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
